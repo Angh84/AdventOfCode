c708862cfa7385996343d88d8195a80cd5d2a5b1
Andreas Hansson <andreashansson84@gmail.com>
2023-12-09 21:15:20 +0100

    Refactor Day07 Puzzle Code

--- AdventOfCode/Puzzles/Day07.cs
+++ AdventOfCode/Puzzles/Day07.cs
@@ -1,295 +1,188 @@
 ﻿using AdventOfCode.Lib;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace AdventOfCode.Puzzles
+namespace AdventOfCode.Puzzles;
+
+public class Day07(IEnumerable<string> puzzleInput) : PuzzleBase(puzzleInput)
 {
-    public class Day07(IEnumerable<string> puzzleInput) : PuzzleBase(puzzleInput)
+    public override string FirstSolution()
     {
-        public override string FirstSolution()
-        {
-            var hands = ParseHands();
-            hands.Sort();
-            var sum = 0;
-            for (var i=0; i< hands.Count; i++)
-            {
-                sum += hands[i].Bid * (i + 1);
-            }
-            return sum.ToString();
-        }
+        var hands = ParseHands();
+        hands.Sort();
+        var sum = hands.Select((t, i) => t.Bid * (i + 1)).Sum();
+        return sum.ToString();
+    }
+    public override string SecondSolution()
+    {
+        var hands = ParseHandsWithJokers();
+        hands.Sort();
+        var sum = hands.Select((t, i) => t.Bid * (i + 1)).Sum();
+        return sum.ToString();
+    }
 
-        public override string SecondSolution()
+    private List<Hand> ParseHands() 
+    {
+        var result = new List<Hand>();
+        Input.ToList().ForEach(x => result.Add(ParseHand(x)));
+        return result;
+    }
+    private List<HandWithJoker> ParseHandsWithJokers()
+    {
+        var result = new List<HandWithJoker>();
+        Input.ToList().ForEach(x => result.Add(ParseHandWithJoker(x)));
+        return result;
+    }
+    private Hand ParseHand(string handString)
+    {
+        var parts = handString.Split(' ');
+        var hand = new Hand(int.Parse(parts[1]));
+        var cardString = parts[0];
+        foreach (var c in cardString)
         {
-            var hands = ParseHandsWithJokers();
-            hands.Sort();
-            var sum = 0;
-            for (var i = 0; i < hands.Count; i++)
+            switch (c)
             {
-                sum += hands[i].Bid * (i + 1);
-            }
-            return sum.ToString();
-        }
-
-        private List<Hand> ParseHands() 
-        {
-            var result = new List<Hand>();
-            Input.ToList().ForEach(x => result.Add(ParseHand(x)));
-            return result;
-        }
-        private List<HandWithJoker> ParseHandsWithJokers()
-        {
-            var result = new List<HandWithJoker>();
-            Input.ToList().ForEach(x => result.Add(ParseHandWithJoker(x)));
-            return result;
-        }
-        private Hand ParseHand(string handString)
-        {
-            var parts = handString.Split(' ');
-            var hand = new Hand(int.Parse(parts[1]));
-            var cardString = parts[0];
-            foreach (char c in cardString)
-            {
-                if (c == 'T')
-                {
+                case 'T':
                     hand.Cards.Add(10);
-                }
-                else if (c == 'J')
-                {
+                    break;
+                case 'J':
                     hand.Cards.Add(11);
-                }
-                else if (c == 'Q')
-                {
+                    break;
+                case 'Q':
                     hand.Cards.Add(12);
-                }
-                else if (c == 'K')
-                {
+                    break;
+                case 'K':
                     hand.Cards.Add(13);
-                }
-                else if (c == 'A')
-                {
+                    break;
+                case 'A':
                     hand.Cards.Add(14);
-                }
-                else
-                {
+                    break;
+                default:
                     hand.Cards.Add(int.Parse(c.ToString()));
-                }
+                    break;
             }
-            return hand;
         }
-        private HandWithJoker ParseHandWithJoker(string handString)
+        return hand;
+    }
+    private HandWithJoker ParseHandWithJoker(string handString)
+    {
+        var parts = handString.Split(' ');
+        var hand = new HandWithJoker(int.Parse(parts[1]));
+        var cardString = parts[0];
+        foreach (var c in cardString)
         {
-            var parts = handString.Split(' ');
-            var hand = new HandWithJoker(int.Parse(parts[1]));
-            var cardString = parts[0];
-            foreach (char c in cardString)
+            switch (c)
             {
-                if (c == 'T')
-                {
+                case 'T':
                     hand.Cards.Add(10);
-                }
-                else if (c == 'J')
-                {
+                    break;
+                case 'J':
                     hand.Cards.Add(1);
-                }
-                else if (c == 'Q')
-                {
+                    break;
+                case 'Q':
                     hand.Cards.Add(12);
-                }
-                else if (c == 'K')
-                {
+                    break;
+                case 'K':
                     hand.Cards.Add(13);
-                }
-                else if (c == 'A')
-                {
+                    break;
+                case 'A':
                     hand.Cards.Add(14);
-                }
-                else
-                {
+                    break;
+                default:
                     hand.Cards.Add(int.Parse(c.ToString()));
-                }
-            }
-            return hand;
-        }
-        private class Hand(int bid) : IComparable<Hand>
-        {
-            public List<int> Cards { get; set; } = [];
-            public int Bid { get; } = bid;
-
-            public int CompareTo(Hand? other)
-            {
-                if (other == null)
-                {
-                    return 1;
-                }
-                else
-                {
-                    if (this.Type() > other.Type())
-                    {
-                        return 1;
-                    }
-                    else if (this.Type() < other.Type())
-                    {
-                        return -1;
-                    }
-                    else
-                    {
-                        for (var i = 0; i < this.Cards.Count; i++)
-                        {
-                            if (this.Cards[i] > other.Cards[i])
-                            {
-                                return 1;
-                            }
-                            else if (this.Cards[i] < other.Cards[i])
-                            {
-                                return -1;
-                            }
-                        }
-                    }
-                }
-                return 0;
-            }
-
-            public HandType Type ()
-            {
-                if (Cards.Distinct().Count() == 1)
-                {
-                    return HandType.FiveOfAKind;
-                }
-                else if (Cards.Distinct().Count() == 2)
-                {
-                    if (Cards.GroupBy(x => x).Any(x => x.Count() == 4))
-                    {
-                        return HandType.FourOfAKind;
-                    }
-                    else
-                    {
-                        return HandType.FullHouse;
-                    }
-                }
-                else if (Cards.Distinct().Count() == 3)
-                {
-                    if (Cards.GroupBy(x => x).Any(x => x.Count() == 3))
-                    {
-                        return HandType.ThreeOfAKind;
-                    }
-                    else
-                    {
-                        return HandType.TwoPair;
-                    }
-                }
-                else if (Cards.Distinct().Count() == 4)
-                {
-                    return HandType.Pair;
-                }
-                else
-                {
-                    return HandType.HighCard;
-                }
+                    break;
             }
         }
-        private class HandWithJoker(int bid): IComparable<HandWithJoker> 
+        return hand;
+    }
+    private class Hand(int bid) : IComparable<Hand>
+    {
+        public List<int> Cards { get; } = [];
+        public int Bid { get; } = bid;
+
+        public int CompareTo(Hand? other)
         {
-            public List<int> Cards { get; set; } = [];
-            public int Bid { get; } = bid;
-            public int CompareTo(HandWithJoker? other)
+            if (other == null) return 1;
+            if (Type() > other.Type()) return 1;
+            if (Type() < other.Type()) return -1;
+            
+            for (var i = 0; i < Cards.Count; i++)
             {
-                if (other == null)
-                {
-                    return 1;
-                }
-                else
-                {
-                    if (this.Type() > other.Type())
-                    {
-                        return 1;
-                    }
-                    else if (this.Type() < other.Type())
-                    {
-                        return -1;
-                    }
-                    else
-                    {
-                        for (var i = 0; i < this.Cards.Count; i++)
-                        {
-                            if (this.Cards[i] > other.Cards[i])
-                            {
-                                return 1;
-                            }
-                            else if (this.Cards[i] < other.Cards[i])
-                            {
-                                return -1;
-                            }
-                        }
-                    }
-                }
-                return 0;
+                if (Cards[i] > other.Cards[i]) return 1;
+                if (Cards[i] < other.Cards[i]) return -1;
             }
-
-            public HandType Type()
-            {
-                int jokerCount = Cards.Count(card => card == 1);
-                var groups = Cards.Where(card => card != 1)
-                                      .GroupBy(card => card)
-                                      .OrderByDescending(group => group.Count()).ToList();
-
-                if (groups.Count == 0) // we got 5 jokers
-                {
-                    return HandType.FiveOfAKind;
-                }
-
-                var uniqueCardCounts = groups.Select(group => group.Count() + jokerCount).ToList();
-                jokerCount -= (uniqueCardCounts[0] - groups[0].Count());
-
-                if (uniqueCardCounts.Count == 1)
-                {
-                    return HandType.FiveOfAKind;
-                }
-
-                if (uniqueCardCounts.Count == 2)
-                {
-                    if (uniqueCardCounts.Contains(4) || uniqueCardCounts.Contains(4 - jokerCount))
-                    {
-                        return HandType.FourOfAKind;
-                    }
-                    else
-                    {
-                        return HandType.FullHouse;
-                    }
-                }
-
-                if (uniqueCardCounts.Count == 3)
-                {
-                    if (uniqueCardCounts.Contains(3) || uniqueCardCounts.Contains(3 - jokerCount))
-                    {
-                        return HandType.ThreeOfAKind;
-                    }
-                    else
-                    {
-                        return HandType.TwoPair;
-                    }
-                }
-
-                if (uniqueCardCounts.Count == 4)
-                {
-                    return HandType.Pair;
-                }
-
-                return HandType.HighCard;
-            }
+            return 0;
         }
-        private enum HandType
+
+        public HandType Type ()
         {
-            FiveOfAKind = 7,
-            FourOfAKind = 6,
-            FullHouse = 5,
-            ThreeOfAKind = 4,
-            TwoPair = 3,
-            Pair = 2,
-            HighCard = 1,
+            return Cards.Distinct().Count() switch
+            {
+                1 => HandType.FiveOfAKind,
+                2 => Cards.GroupBy(x => x).Any(x => x.Count() == 4) 
+                    ? HandType.FourOfAKind 
+                    : HandType.FullHouse,
+                3 => Cards.GroupBy(x => x).Any(x => x.Count() == 3) 
+                    ? HandType.ThreeOfAKind 
+                    : HandType.TwoPair,
+                4 => HandType.Pair,
+                _ => HandType.HighCard
+            };
         }
     }
-    
+    private class HandWithJoker(int bid): IComparable<HandWithJoker> 
+    {
+        public List<int> Cards { get; } = [];
+        public int Bid { get; } = bid;
+        public int CompareTo(HandWithJoker? other)
+        {
+            if (other == null) return 1;
+            if (Type() > other.Type())return 1;
+            if (Type() < other.Type())return -1;
+            for (var i = 0; i < Cards.Count; i++)
+            {
+                if (Cards[i] > other.Cards[i]) return 1;
+                if (Cards[i] < other.Cards[i]) return -1;
+            }
+            return 0;
+        }
+
+        public HandType Type()
+        {
+            var jokerCount = Cards.Count(card => card == 1);
+            var groups = Cards.Where(card => card != 1)
+                                  .GroupBy(card => card)
+                                  .OrderByDescending(group => group.Count()).ToList();
+
+            if (groups.Count == 0) 
+            {
+                return HandType.FiveOfAKind;
+            }
+
+            var uniqueCardCounts = groups.Select(group => group.Count() + jokerCount).ToList();
+            jokerCount -= uniqueCardCounts[0] - groups[0].Count();
+
+            return uniqueCardCounts.Count switch
+            {
+                1 => HandType.FiveOfAKind,
+                2 when uniqueCardCounts.Contains(4) || uniqueCardCounts.Contains(4 - jokerCount) =>
+                    HandType.FourOfAKind,
+                2 => HandType.FullHouse,
+                3 when uniqueCardCounts.Contains(3) || uniqueCardCounts.Contains(3 - jokerCount) => HandType
+                    .ThreeOfAKind,
+                3 => HandType.TwoPair,
+                4 => HandType.Pair,
+                _ => HandType.HighCard
+            };
+        }
+    }
+    private enum HandType
+    {
+        FiveOfAKind = 7,
+        FourOfAKind = 6,
+        FullHouse = 5,
+        ThreeOfAKind = 4,
+        TwoPair = 3,
+        Pair = 2,
+        HighCard = 1,
+    }
 }
+
